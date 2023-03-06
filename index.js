d3c4eca2db45bc67185ae4e7fe61f7ce05d62ee7
JosuaLimbu <osaltusal@gmail.com>
2023-03-06 10:50:37 +0800

    Back End

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ app.get('/', (req, res) => {
     res.send('This is the home page');
 });
 app.get('/about', (req, res) => {
+    const date = new Date().toISOString();
     res.json({
         status: 'success',
         message: 'response success',
         description: 'Exercise #03',
-        date: `${new Date().toISOString()}`,
+        date: date,
         data: members
     })
 });
@@ -21,6 +22,13 @@ app.get('/users', async (req, res) => {
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify(await getUsers()));
   });
+// app.get("/post/:id", (req, res) => {
+//     let id = req.params.id;
+//     res.json({
+//         id,
+//     });
+// });
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 })
